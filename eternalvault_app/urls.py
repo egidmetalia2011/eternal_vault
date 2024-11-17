b12356dc29f2b2f051099d35ade7147e81d086d3
egidmetalia2011 <95558023+egidmetalia2011@users.noreply.github.com>
2024-11-16 21:11:22 -0500

    Last Minute CHAGE

--- eternalvault_app/urls.py
+++ eternalvault_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('upload/', views.upload_file_view, name='upload_file'),
+    path('download/<int:secure_data_id>', views.download_file_view, name='download_file'),
 ]
--- eternalvault_app/views.py
+++ eternalvault_app/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 from .models import SecureData, UserProfile
 from django.contrib.auth.decorators import login_required
 from django.utils.timezone import now
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404
 import logging
 import datetime
 
@@ -13,6 +15,36 @@ def home_view(request):
     return render(request, 'eternalvault_app/home.html')
 
 logger = logging.getLogger(__name__)
+
+@login_required
+def download_file_view(reques, secure_data_id):
+    #get the secure data object
+    secure_data = get_object_or_404(SecureData, id=secure_data_id, user=request.user)
+
+    #get the associated user profile to retrieve the encrypted key
+    try:
+        user_profile = request.user.userprofile
+    except:
+        logger.error(f"UserProfile not found for user: {request.user}")
+        raise Http404("UserProfile not found")
+    
+    #chek if the file can be accessed based on target_date
+    if now().date() < user_profile.target_date:
+        logger.warning(f"access attempt before target date for file: {secure_data.title} by user: {request.user}")
+        raise Http404("this file can not be accessed before the target date")
+
+    #decryopt the file using the user's encryption key
+    try:
+        decrypt_content = secure_data.get_decrypted_file(user_profile.encrypted_key)
+    except:
+        logger.error(f"error decrypting file: {secure_data.title} for user: {request.user}, error: {e}")
+        raise Http404("error decrypting file")
+    
+    #create a response with the decrypted content
+    response = HttpResponse(decrypted_content, content_type='application/octet-stream')
+    response['Content-Disposition'] = f'attachment; filename="{secure_data.title}"'
+    return response
+
 
 @login_required
 def upload_file_view(request):
